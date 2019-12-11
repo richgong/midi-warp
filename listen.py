28721e9b76224f3b800368be9b1b6b698ec5cde5
Richard Gong <richard.gong@instacart.com>
2019-12-10 21:14:41 -0800

    print outputs

--- listen.py
+++ listen.py
@@ -1,7 +1,7 @@
 import time
 from mido.backends.backend import Backend
 from mido.messages.messages import Message
-from mido.backends.rtmidi import Input
+from mido.backends.rtmidi import Input, Output
 
 
 backend = Backend()
@@ -21,7 +21,8 @@ class Inst:
             self.input = backend.open_input(self.name, callback=self.input_callback)
 
     def input_callback(self, note_in):
-        print('[input]', self, note_in, f'/ ableton_channel={note_in.channel + 1}' if hasattr(note_in, 'channel') else '')
+        if note_in.type != 'clock':
+            print('[input]', self, note_in, f'/ real_channel={note_in.channel + 1}' if hasattr(note_in, 'channel') else '')
         if note_in.type in ['note_on', 'note_off']:
             octave = int(note_in.note / 12)
             key = note_in.note % 12
@@ -31,10 +32,15 @@ class Inst:
 
 
 input_names = backend.get_input_names()
+output_names = backend.get_output_names()
 
-print("All inputs:", input_names)
+print("All readable inputs:", input_names)
 for i in range(len(input_names)):
     Inst(index=i, is_out=False)
+
+print("All writable outputs:", output_names)
+#for i in range(len(output_names)):
+#    Inst(index=i, is_out=True)
 
 while True:
     time.sleep(100)
